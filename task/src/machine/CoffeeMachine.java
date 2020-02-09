959854814d1c029c3e0242f8e7ea71dc506f61d2
Tobias Grunwald <tobiasgrunwald92@gmail.com>
2020-02-09 10:54:11 +0100

    completed project

--- task/src/machine/CoffeeMachine.java
+++ task/src/machine/CoffeeMachine.java
@@ -3,9 +3,6 @@ import java.util.*;
 
 public class CoffeeMachine {
     static Scanner scanner = new Scanner(System.in);
-    final static int NEEDED_WATER = 200;
-    final static int NEEDED_MILK = 50;
-    final static int NEEDED_BEANS = 15;
     int water;
     int milk;
     int beans;
@@ -118,34 +115,6 @@ public class CoffeeMachine {
         System.out.println("I have enough resources, making you a coffee!");
         return true;
     }
-
-    //Legacy Code from prvious exercises that might be needed again
-    /*
-    public void makeCoffee(int cups){
-        int maxCups = maxAmount();
-        if (ressourcesAvailable(cups)) {
-            StringBuilder message = new StringBuilder();
-            message.append("Yes, I can make that amount of coffee");
-            if (maxCups > cups) {
-                message.append(" (and even " + ( maxCups - cups) + " more than that)");
-            }
-            System.out.println(message.toString());
-        } else {
-            System.out.println("No, I can make only " + maxCups + " cup(s) of coffee");
-        }
-
-    public boolean ressourcesAvailable(int cups){
-        return (water >= NEEDED_WATER * cups && milk > NEEDED_MILK * cups && beans > NEEDED_BEANS * cups);
-    }
-
-    public int maxAmount() {
-        int maxWater = water/NEEDED_WATER ;
-        int maxMilk = milk/NEEDED_MILK;
-        int maxBeans = beans/NEEDED_BEANS;
-        //Which one is empty first?
-        return Math.min(Math.min(maxWater, maxMilk), maxBeans);
-    }
-     */
 
     public static String input(String question){
         System.out.println(question);
